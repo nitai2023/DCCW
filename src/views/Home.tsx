c9f395f2e92e896c72cae1aea842629326f525db
admin <3251736481@qq.com>
2024-05-11 17:15:06 +0800

    主页和登录页面

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { HashRouter as Router, Link } from 'react-router-dom';
+import styles from './Home.module.scss';
 // 主页
 function Home() {
   return (
     <Router>
-      <div>
-        <h1>逐梦校园管理端</h1>
-        <Link to="/Login">
-          <Button variant="contained">登录</Button>
-        </Link>
+      <div className={styles.body}>
+        <div className={styles.main}>
+          <h1>DCCW</h1>
+          <h2>逐梦校园管理端</h2>
+          <Link to="/Login">
+            <Button type="submit" variant="contained" color="success">
+              登录
+            </Button>
+          </Link>
+        </div>
       </div>
     </Router>
   );
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { Button, Input, FormGroup, TextField } from '@mui/material';
 import { HashRouter as Router, Link } from 'react-router-dom';
+import styles from './Login.module.scss';
 // 登录界面
 function Login() {
   return (
     <Router>
-      <div>
-        <Link to="/">
-          <Button variant="contained">回到主页</Button>
-        </Link>
+      <div className={styles.body}>
+        <div className={styles.form}>
+          <FormGroup>
+            <div className={styles.MuiFormGroup_row}>
+              <label>用户名</label>
+              <TextField required id="username" label="username" />
+            </div>
+            <div className={styles.MuiFormGroup_row}>
+              <label>密码</label>
+              <TextField required id="password" label="password" />
+            </div>
+
+            <div className={styles.MuiFormGroup_row}>
+              <Button type="submit" variant="contained" color="success">
+                登录
+              </Button>
+              <Link to="/">
+                <Button type="submit" variant="contained" color="primary">
+                  回到主页
+                </Button>
+              </Link>
+            </div>
+          </FormGroup>
+        </div>
       </div>
     </Router>
   );
